89bc3410e8c031530460183dc2ca8e91fe53e340
Daniel <102876965+Dauniel@users.noreply.github.com>
2022-08-24 23:08:59 -0700

    Add files via upload

---
+++ countdown.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+using namespace std;
+
+int main () {
+
+int num, num2,i;
+
+cin >> num;
+num2 = num;
+
+for (i = 1; i <= num; i++) {
+
+num2--;
+cout << num2 << "\n";
+
+}
+
+
+
+
+
+
+
+
+
+}
